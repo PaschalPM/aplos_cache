723814f73264f434b7d4e90b5f9e7c4356342ac1
Paschal Okafor <okaforpaschal018@gmail.com>
2023-05-27 08:27:54 +0100

    Minor Refactor

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from .src.file_cache import FileCache
-from os.path import realpath, relpath, abspath
+from os.path import realpath, relpath, abspath, exists, join
+from os import makedirs
 import re
 
 class Cache(FileCache):
 
-    storage_dir = '/storage' # path to cache storage_directory
+    storage_dir = 'storage' # path to cache storage_directory
     file_name = 'cache'
 
     __cache_object = None
@@ -12,24 +13,28 @@ class Cache(FileCache):
     def __init__(self, storage_dir=None, file_name=None):
 
         self.__configure(storage_dir, file_name)
-        self.__cache_object = FileCache(relpath('.')+self.storage_dir, self.file_name)
+        self.__cache_object = FileCache(self.storage_dir, self.file_name)
         
 
     def __configure(self, storage_dir, file_name):
+        ''' Configures the storage_directory of this cache instance '''
 
-        ''' Configures the storage storage_directory of this cache instance '''
         if not storage_dir:
             self.storage_dir = Cache.storage_dir
         else:
             self.storage_dir = storage_dir
 
+        self.storage_dir = join(abspath('.'), self.storage_dir)
+
+        makedirs(self.storage_dir, exist_ok=True)
+        
         ''' Configures the storage file/db name of this cache instance '''
         if not file_name:
             self.file_name = Cache.file_name
         else:
             self.file_name = file_name
 
-
+    
     def has(self, key):
         '''
             Checks if data (key, value) exists in file/db cache storage
